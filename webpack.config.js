494f4c4910778369357cdea7e7e36f9fc25143c7
Igor Kim <igkim90@gmail.com>
2018-11-07 03:31:37 +0500

    added file-loader config, remove default settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'template.html',
@@ -23,6 +18,16 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
         },
       },
     ],
