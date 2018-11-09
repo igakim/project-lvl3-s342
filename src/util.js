22d580924a5774b9b90d60e0f1985bd86583d2c9
Igor Kim <igkim90@gmail.com>
2018-11-09 20:49:42 +0500

    add getError function

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 
-const getArticleDescription = (target, state) => {
+export const getArticleDescription = (target, state) => {
   const { feedName } = target.dataset;
   const articleLink = target.dataset.itemLink;
   const actualFeed = state.responses.find(e => e.title === feedName);
@@ -7,4 +7,17 @@ const getArticleDescription = (target, state) => {
   return description;
 };
 
-export default getArticleDescription;
+export const getError = (err) => {
+  let errorDescription = '';
+  if (err.response) {
+    errorDescription = `${err.response.status} ${err.response.statusText}`;
+  } else if (err.request) {
+    errorDescription = 'The request was made but no response was received';
+  } else {
+    errorDescription = 'Something happened in setting up the request that triggered an Error';
+  }
+  return {
+    title: 'Couldn\'t load RSS feed!',
+    errorDescription,
+  };
+};
