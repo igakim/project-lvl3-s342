6ec6ec357567ceaa7a2fe01f66be20376a7a183b
Igor Kim <igkim90@gmail.com>
2018-11-09 12:16:11 +0500

    separate error parse and data parse

--- src/parse.js
+++ src/parse.js
@@ -1,22 +1,23 @@
-export default (data, err) => {
-  if (data) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(data, 'application/xml');
-    const items = [...doc.getElementsByTagName('item')];
-    const mappedItems = items.map((el) => {
-      const newItem = {
-        title: el.querySelector('title').textContent,
-        link: el.querySelector('link').textContent,
-        description: el.querySelector('description').textContent,
-      };
-      return newItem;
-    });
-    return {
-      title: doc.querySelector('title').textContent,
-      chanelDescription: doc.querySelector('description').textContent,
-      items: mappedItems,
+export const responseParse = (data) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, 'application/xml');
+  const items = [...doc.getElementsByTagName('item')];
+  const mappedItems = items.map((el) => {
+    const newItem = {
+      title: el.querySelector('title').textContent,
+      link: el.querySelector('link').textContent,
+      description: el.querySelector('description').textContent,
     };
-  }
+    return newItem;
+  });
+  return {
+    title: doc.querySelector('title').textContent,
+    chanelDescription: doc.querySelector('description').textContent,
+    items: mappedItems,
+  };
+};
+
+export const errorParse = (err) => {
   let errorDescription = '';
   if (err.response) {
     errorDescription = `${err.response.status} ${err.response.statusText}`;
@@ -29,4 +30,4 @@ export default (data, err) => {
     title: 'Couldn\'t load RSS feed!',
     errorDescription,
   };
-}
+};
