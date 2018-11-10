377612999864d85989552771a50a38b92fb23863
Igor Kim <igkim90@gmail.com>
2018-11-10 16:49:33 +0500

    refactor state's name

--- src/util.js
+++ src/util.js
@@ -2,7 +2,7 @@
 export const getArticleDescription = (target, state) => {
   const { feedName } = target.dataset;
   const articleLink = target.dataset.itemLink;
-  const actualFeed = state.responses.find(e => e.title === feedName);
+  const actualFeed = state.feeds.find(e => e.title === feedName);
   const { description } = actualFeed.items.find(e => e.link === articleLink);
   return description;
 };
